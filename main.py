8704775913cbecfe1862899910a05baf789e8134
Mauricio Azevedo Franco Ladeira <mauricioafl001@gmail.com>
2023-01-20 14:58:22 -0300

    alterando nome do metodo de print valor indiviudal

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import math
 
 destino = ['Afeganistão',
-               'Argélia',
-               'Brasil',
-               'Canadá',
-               'França',
-               'Gana',
-               'Hungria',
-               'Indonesia',
-               'Japão',
-               'Líbano',
-               'Malásia',
-               'Marrocos',
-               'México',
-               'Nepal',
-               'Paquistão',
-               'Peru',
-               'Portugal',
-               'Quênia',
-               'Romenia',
-               'Ruanda',
-               'Samoa',
-               'Singapura',
-               'Tunisia',
-               'Uruguai',
-               'Zimbabue']
+           'Argélia',
+           'Brasil',
+           'Canadá',
+           'França',
+           'Gana',
+           'Hungria',
+           'Indonesia',
+           'Japão',
+           'Líbano',
+           'Malásia',
+           'Marrocos',
+           'México',
+           'Nepal',
+           'Paquistão',
+           'Peru',
+           'Portugal',
+           'Quênia',
+           'Romenia',
+           'Ruanda',
+           'Samoa',
+           'Singapura',
+           'Tunisia',
+           'Uruguai',
+           'Zimbabue']
 
 origem = ['Brasil',
           'Canadá',
@@ -34,20 +34,26 @@ origem = ['Brasil',
           'Zimbabue']
 
 custos = [
-    [6.69,9.55,0.00,8.28,5.10,5.41,5.73,7.96,7.64,8.92,4.78,5.10,3.82,8.28,8.60,8.92,3.50,4.78,6.69,4.78,4.14,3.18,4.14,6.37,4.46, 999],
-    [8.92,5.41,3.50,0.00,8.60,7.64,4.14,7.96,8.92,9.55,5.73,7.96,5.73,8.60,9.55,4.14,4.14,7.01,7.96,6.69,3.50,7.96,4.14,7.32,7.01, 999],
-    [5.73,6.05,6.69,3.50,0.00,3.50,7.01,8.92,4.14,9.55,8.28,6.05,8.28,8.60,9.24,8.28,3.18,4.78,5.73,8.92,5.41,7.64,6.69,4.14,5.41, 999],
-    [6.05,6.69,3.18,7.01,6.37,9.55,5.73,6.37,0.00,6.05,8.60,7.32,7.96,3.50,6.37,9.24,3.18,3.18,4.14,4.14,6.05,5.10,4.46,9.55,7.32, 999],
-    [5.41,4.46,6.05,9.55,4.78,8.92,5.10,8.60,4.14,8.92,4.14,4.78,4.14,6.37,3.18,3.50,6.05,5.10,5.73,3.50,7.64,0.00,6.69,4.78,4.78, 999],
-    [7.96,3.82,5.41,9.55,6.69,9.24,7.96,6.37,7.96,3.50,8.28,5.73,9.55,5.73,9.24,5.73,6.05,8.60,7.64,3.18,7.64,6.37,7.96,9.55,0.00, 999]
-          
-    ]
+    [6.69, 9.55, 0.00, 8.28, 5.10, 5.41, 5.73, 7.96, 7.64, 8.92, 4.78, 5.10, 3.82,
+        8.28, 8.60, 8.92, 3.50, 4.78, 6.69, 4.78, 4.14, 3.18, 4.14, 6.37, 4.46, 999],
+    [8.92, 5.41, 3.50, 0.00, 8.60, 7.64, 4.14, 7.96, 8.92, 9.55, 5.73, 7.96, 5.73,
+        8.60, 9.55, 4.14, 4.14, 7.01, 7.96, 6.69, 3.50, 7.96, 4.14, 7.32, 7.01, 999],
+    [5.73, 6.05, 6.69, 3.50, 0.00, 3.50, 7.01, 8.92, 4.14, 9.55, 8.28, 6.05, 8.28,
+        8.60, 9.24, 8.28, 3.18, 4.78, 5.73, 8.92, 5.41, 7.64, 6.69, 4.14, 5.41, 999],
+    [6.05, 6.69, 3.18, 7.01, 6.37, 9.55, 5.73, 6.37, 0.00, 6.05, 8.60, 7.32, 7.96,
+        3.50, 6.37, 9.24, 3.18, 3.18, 4.14, 4.14, 6.05, 5.10, 4.46, 9.55, 7.32, 999],
+    [5.41, 4.46, 6.05, 9.55, 4.78, 8.92, 5.10, 8.60, 4.14, 8.92, 4.14, 4.78, 4.14,
+        6.37, 3.18, 3.50, 6.05, 5.10, 5.73, 3.50, 7.64, 0.00, 6.69, 4.78, 4.78, 999],
+    [7.96, 3.82, 5.41, 9.55, 6.69, 9.24, 7.96, 6.37, 7.96, 3.50, 8.28, 5.73, 9.55,
+        5.73, 9.24, 5.73, 6.05, 8.60, 7.64, 3.18, 7.64, 6.37, 7.96, 9.55, 0.00, 999]
+
+]
 
 
 disponibilidade = [7500, 8100, 4900, 5300, 3900, 3600]
 
 necessidade = [1673, 963, 1444, 1282, 2193, 782, 683, 906, 1195, 962, 1782, 1147,
-        1711, 1441, 1878, 1172, 1254, 548, 643, 1986, 929, 2173, 1907, 1908, 666, 72]
+               1711, 1441, 1878, 1172, 1254, 548, 643, 1986, 929, 2173, 1907, 1908, 666, 72]
 
 
 resultado_custos = []
@@ -133,7 +139,6 @@ def min_positivo(iteravel):
             raise Exception('')
     except:
         exit(1)
-        
 
 
 def encontra_menor_celula(penalidade_origem, penalidade_destino):
@@ -147,11 +152,13 @@ def encontra_menor_celula(penalidade_origem, penalidade_destino):
             maior_penalidade_destino)
         resultado.append(index_maior_diferenca)
         coluna = get_coluna(index_maior_diferenca)
-        menor_valor_custo = min_positivo(iteravel_sem_nulo(coluna, disponibilidade))
+        menor_valor_custo = min_positivo(
+            iteravel_sem_nulo(coluna, disponibilidade))
         resultado.append(menor_valor_custo)
         resultado.append(coluna.index(menor_valor_custo))
     else:
-        index_maior_diferenca = penalidade_origem.index(maior_penalidade_origem)
+        index_maior_diferenca = penalidade_origem.index(
+            maior_penalidade_origem)
         resultado.append(index_maior_diferenca)
         linha = custos[index_maior_diferenca]
         menor_valor_custo = min_positivo(iteravel_sem_nulo(linha, necessidade))
@@ -169,15 +176,17 @@ def calcula_resultado():
             z += resultado_custos[i][j]
     return z
 
-def aaaa():
-    aux=0
+
+def valor_transporte_individual():
+    aux = 0
     for i in range(0, len(resultado_custos)):
         for j, num in enumerate(resultado_custos[aux]):
             if num != 0:
-                a=origem[i]
-                b=destino[j-1]
-                print(str(a)+" --> "+str(b)+" = "+ str(num))
-        aux=aux+1
+                a = origem[i]
+                b = destino[j-1]
+                print(str(a)+" --> "+str(b)+" = " + str(num))
+        aux = aux+1
+
 
 def main():
 
@@ -210,7 +219,7 @@ def main():
 main()
 # print(resultado_custos)
 print('===================================')
-print('Transportes:') 
-aaaa()     
+print('Transportes:')
+valor_transporte_individual()
 print('===================================')
 print(calcula_resultado())
